e01d18c1f1fa1c217470f4daae42a37dd14a1305
Nelson Modenez Neto <99834482+NelsonModenezNeto@users.noreply.github.com>
2023-05-23 09:42:16 -0300

    oi
    oi

--- Flutter/ex2/lib/about.dart
+++ Flutter/ex2/lib/about.dart
@@ -1,12 +1,37 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 
 class About extends StatelessWidget {
-  const About({super.key});
+  const About({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 30),
+              ClipOval(
+                child: Image.network(
+                  "https://img.freepik.com/fotos-gratis/empresario-prospero-mantem-as-maos-cruzadas-tem-expressao-satisfeita_273609-16711.jpg",
+                  width: 500,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(16),
+                child: Text(
+                  "Monteiro Lobato (1882-1948) foi um escritor e editor brasileiro. O Sítio do Pica-pau Amarelo é sua obra de maior destaque na literatura infantil. Criou a Editora Monteiro Lobato e mais tarde a Companhia Editora Nacional. Foi um dos primeiros autores de literatura infantil de nosso país e de toda a América Latina. Ao lado da literatura infantil, Monteiro Lobato também deixou uma extensa obra voltada para o público adulto. Retratou os vilarejos decadentes e a população do Vale do Paraíba, quando da crise do café.",
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- Flutter/ex2/lib/cont.dart
+++ Flutter/ex2/lib/cont.dart
@@ -2,11 +2,100 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 
-class Cont extends StatelessWidget {
+
+
+import 'package:flutter/material.dart';
+
+class Cont extends StatefulWidget {
   const Cont({super.key});
 
   @override
+  _ContState createState() => _ContState();
+}
+
+class _ContState extends State<Cont> {
+  int _contador = 0;
+
+  void somar() {
+    setState(() {
+      _contador++;
+    });
+  }
+
+  void subtrair() {
+    setState(() {
+      if(_contador == 0){
+        _contador == 0;
+      }
+      else{
+      _contador--;
+      }
+    });
+  }
+
+  int getContador() {
+    return _contador;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.network(
+                "https://media.istockphoto.com/id/1173585579/pt/foto/brigadier.jpg?s=612x612&w=0&k=20&c=huBBKQPsrmdntZbdmUOKvP_C_VY2RJsfFivRSTJ9kcA=",
+                width: 100,
+                height: 100,
+              ),
+              Text(
+                'Contador:',
+              ),
+              Text(
+                getContador().toString(),
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FloatingActionButton(
+                    child: const Icon(Icons.remove),
+                    onPressed: () {
+                      setState(() {
+                        subtrair();
+                      });
+                    },
+                  ),
+                  SizedBox(width: 16),
+                  FloatingActionButton(
+                    child: const Icon(Icons.add),
+                    onPressed: () {
+                      setState(() {
+                        somar();
+                      });
+                    },
+                  ),
+                ],
+              ),
+              Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+              SizedBox(height: 80),
+                Text("O número de brigadeiros é: "+ getContador().toString(),
+                style: TextStyle(fontSize: 18),
+                )
+              ],)
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
