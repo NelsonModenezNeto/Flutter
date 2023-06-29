0a7e483d6c5c5023f83ad63a74f57cb3e9a2c6af
Nelson Modenez Neto <nelsonmodenezneto@hotmail.com>
2023-06-28 21:20:26 -0300

    q
    q

--- Flutter/flutter_application_1/lib/main.dart
+++ Flutter/flutter_application_1/lib/main.dart
@@ -1,8 +1,5 @@
-// ignore_for_file: prefer_const_constructors
-
-import 'package:flutter/material.dart';
-
 import 'my_dog.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,65 +11,39 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text("Bem Vindo"),
-            backgroundColor: Colors.blue,
-            centerTitle: true,
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+      ),
+      routes: {
+        '/operacoes': (context) => const my_dog(),
+      },
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Operação'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/my_dog');
+            },
+            icon: const Icon(Icons.numbers_rounded),
           ),
-          body: Center(
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              MyDog(
-                  "Bem Vindo(a) à aula de desenvovimento de aplicativos para dispositivos móveis",
-                  Colors.transparent,
-                  18,
-                  1),
-              SizedBox(height: 60),
-              MyDog("Aqui você pode encontrar:", Colors.transparent, 12, 1),
-              SizedBox(height: 60),
-              Row(
-                children: [
-                  Image.network(
-                    "https://www.thinkdata.com.br/wp-content/uploads/2019/02/think-data-lanca-solucao-de-checagem-de-enderecos-com-foco-na-concessao-de-credito-televendas-cobranca.jpg",
-                    width: 50,
-                    height: 50,
-                  ),
-                  MyDog("Tomé", Colors.blue, 50, 30),
-                  SizedBox(height: 30),
-                  Image.network(
-                    "https://www.thinkdata.com.br/wp-content/uploads/2019/02/think-data-lanca-solucao-de-checagem-de-enderecos-com-foco-na-concessao-de-credito-televendas-cobranca.jpg",
-                    width: 50,
-                    height: 50,
-                  ),
-                  MyDog("Tomé", Colors.blue, 50, 30),
-                  SizedBox(height: 30),
-                  Image.network(
-                    "https://www.thinkdata.com.br/wp-content/uploads/2019/02/think-data-lanca-solucao-de-checagem-de-enderecos-com-foco-na-concessao-de-credito-televendas-cobranca.jpg",
-                    width: 50,
-                    height: 50,
-                  ),
-                  MyDog("Tomé", Colors.blue, 50, 30),
-                ],
-              )
-            ]),
-          ),
-          bottomNavigationBar: BottomAppBar(
-            color: Colors.blue,
-            child: SizedBox(
-              height: 60,
-              child: Align(
-                  child: Text(
-                'Venha Estudar Conosco!',
-                style: TextStyle(fontSize: 18, color: Colors.white),
-                textAlign: TextAlign.center,
-              )),
-            ),
-          ),
-        ));
+        ],
+      ),
+      body: Center(
+        child: Text('Página inicial'),
+      ),
+    );
   }
 }
--- Flutter/flutter_application_1/lib/my_dog.dart
+++ Flutter/flutter_application_1/lib/my_dog.dart
@@ -1,27 +1,117 @@
-
-import 'dart:ffi';
-
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 
-class MyDog extends StatelessWidget {
+class my_dog extends StatefulWidget {
+  const my_dog({Key? key});
 
-  //atributos
-  final String nome;
-  final Color cor;
-  final double tamanhoTexto; 
-  final double tamanhoBorda;
+  @override
+  State<my_dog> createState() => _OperacoesState();
+}
 
-  const MyDog(this.nome,  this.cor,  this.tamanhoTexto,  this.tamanhoBorda, {super.key});
+class _OperacoesState extends State<my_dog> {
+  double valor1 = 0;
+  double valor2 = 0;
+  double resultado = 0;
+  TextEditingController campoTexto = TextEditingController();
+  TextEditingController campoTexto2 = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return DecoratedBox(
-        decoration: BoxDecoration(color: cor),
-        child: Padding(
-          padding: EdgeInsets.all(tamanhoBorda),
-          child: Text(nome, style: TextStyle(fontSize: tamanhoTexto),
-        )));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Operações'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            TextField(
+              controller: campoTexto,
+              onChanged: (value) {
+                print(value);
+              },
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 16.0,
+                fontWeight: FontWeight.normal,
+              ),
+              decoration: InputDecoration(
+                labelText: 'Valor 1',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              controller: campoTexto2,
+              onChanged: (value) {
+                print(value);
+              },
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 16.0,
+                fontWeight: FontWeight.normal,
+              ),
+              decoration: InputDecoration(
+                labelText: 'Valor 2',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    valor1 = double.parse(campoTexto.text);
+                    valor2 = double.parse(campoTexto2.text);
+                    resultado = valor1 + valor2;
+                    setState(() {});
+                  },
+                  child: const Text("+"),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    valor1 = double.parse(campoTexto.text);
+                    valor2 = double.parse(campoTexto2.text);
+                    resultado = valor1 - valor2;
+                    setState(() {});
+                  },
+                  child: const Text("-"),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    valor1 = double.parse(campoTexto.text);
+                    valor2 = double.parse(campoTexto2.text);
+                    resultado = valor1 * valor2;
+                    setState(() {});
+                  },
+                  child: const Text("*"),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    valor1 = double.parse(campoTexto.text);
+                    valor2 = double.parse(campoTexto2.text);
+                    resultado = valor1 / valor2;
+                    setState(() {});
+                  },
+                  child: const Text("/"),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                resultado = 0;
+                campoTexto.clear();
+                campoTexto2.clear();
+                setState(() {});
+              },
+              child: const Text("CE"),
+            ),
+            SizedBox(height: 10),
+            Text("O resultado é $resultado"),
+          ],
+        ),
+      ),
+    );
   }
 }
